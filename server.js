03a13f9b467af94e347650b41a4d60df087c5ca9
Douglas Dantas <douglas.mdantas@hotmail.com>
2019-02-22 00:23:31 -0300

    Adicionadas todas as páginas, feito módulo de adicionar participante, listar participante e de fazer o sorteio

--- server.js
+++ server.js
@@ -9,11 +9,59 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000);
+Mongodb.connect(uri, (err, client) =>{
+    if (err) {
+        return console.log(err);
+    }
+
+    db = client.db('douglasdb');
+
+    app.listen(3000);
+
+})
 
 app.get('/', (req, res) =>{
     res.render('index.ejs');
 });
 
+app.get('/novo', (req, res) =>{
+    res.render('novo.ejs');
+})
 
+app.post('/novo', (req, res) =>{
+    db.collection('sorteio').insertOne(req.body, (err, results) =>{
+        if (err) {
+            return console.log(err);
+        }
+
+        res.redirect('/novo');
+    });
+})
+
+app.get('/atual', (req, res) =>{
+    db.collection('sorteio').find().toArray((err, results) =>{
+        if (err) {
+            return console.log(err);
+        }
+
+        res.render('atual.ejs', { data: results });
+    })
+})
+
+app.get('/sorteio', (req, res) =>{
+    db.collection('sorteio').find().toArray((err, results) =>{
+        if (err) {
+            return console.log(err);
+        }
+
+        let resultado = [];
+
+        results.forEach(function(results){
+            resultado.push(results.name);
+        })
+        let sorteado = resultado[Math.floor(Math.random * resultado.length)];
+        console.log(resultado[0]);
+        res.render('sorteio.ejs', { data: sorteado });
+    })
+})
 
